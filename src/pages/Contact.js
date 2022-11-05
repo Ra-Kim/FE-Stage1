4ef697c30a0e5fbfb328eae5222be054533aa6e0
Increase <increasechris124@gmail.com>
2022-11-05 23:26:43 +0100

    style: rounded up styling and error handling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { useForm } from 'react-hook-form'
 
 const Contact = () => {
 
@@ -10,7 +11,15 @@ const Contact = () => {
     agreement: false
 })
 
-let disabled = data.agreement && data.firstName.length && data.email.length && data.message.length
+let disabled = data.agreement && data.firstName.length && data.email.length 
+
+const {register, handleSubmit, formState : {errors}, } = useForm({
+  mode: "onTouched"
+})
+
+const onSubmit = (data) => {
+  console.log(data, "hey")
+}
 
 function handleChange(event){
   const {value, type, name, checked} = event.target
@@ -25,56 +34,62 @@ function handleChange(event){
         <h1>Contact Me</h1>
         <p>Hi there, contact me to ask me about anything you have in mind.</p>
         <div className='form-container'>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className = "name-form-div">
               <div className = "form-item">
-                <label for="first_name">First name</label>
+                <label htmlFor="first_name">First name</label>
                 <input 
                   type="text" 
                   id="first_name" 
                   name="firstName"
-                  onChange={handleChange} 
                   placeholder="Enter your first name" 
                   value = {data.firstName}
-                  required
+                  {...register("firstName", {required:false})}
+                  onChange={handleChange} 
                   />
+                  <p>{errors.firstName && "error message"}</p>
               </div>
               <div className = "form-item">
-                <label for="last_name">Last Name</label>
+                <label htmlFor="last_name">Last Name</label>
                 <input 
                   type="text" 
                   id="last_name" 
                   name="lastName" 
-                  onChange={handleChange} 
                   placeholder="Enter your last name" 
                   value = {data.lastName}
+                  {...register("lastName", {required:false})}
+                  onChange={handleChange} 
                   />
+                  <p>{errors.lastName && "error message"}</p>
               </div>
             </div>
             <div className = "form-item">
-                <label for="email">Email</label>
+                <label htmlFor="email">Email</label>
                 <input 
                   type="text" 
                   id="email" 
                   name="email" 
-                  onChange={handleChange} 
                   placeholder="yourname@email.com" 
                   value = {data.email}
-                  required
+                  {...register("email", {required:false})}
+                  onChange={handleChange} 
                   />
+                  <p>{errors.email && "error message"}</p>
               </div>
               <div className='form-item'>
-                <label htmlFor = "other">Message</label>
+                <label htmlFor = "message">Message</label>
                 <textarea 
                   name="message" 
                   id="message"
                   value = {data.message}
-                  onChange ={handleChange}
+                  style = {{border: errors.message && "1px solid red"}}
                   // className='form-input'
                   placeholder="Send me a message and I'll reply you as soon as possible.."
                   autoComplete="off"
-                  required
+                  {...register("message", {required:true})}
+                  onChange ={handleChange}
                />
+                  <p>{errors.message && "Please enter a message"}</p>
                </div>
                <div className='form-item' id = "checkbox">
                 <input 
@@ -82,21 +97,23 @@ function handleChange(event){
                   id="agreement" 
                   name="agreement" 
                   checked={data.agreement} 
+                  {...register("agreement", {required:true})}
                   onChange= {handleChange} 
-                  required/>
-                <label for="agreement">You agree to submitting your data to Stealth who may contact you</label>
+                />
+                <label htmlFor="agreement">You agree to submitting your data to Stealth who may contact you</label>
                </div>
               <div className='form-item' id='button-div'>
-                <input 
-                  type= "submit" 
+                <button 
                   id='btn_submit'
-                  value ="Send message" 
+                  type= "submit" 
                   className = "button" 
-                  disabled={disabled} 
+                  disabled={!disabled} 
                   style = {{
                       background: (!disabled) ? "#B2DDFF" : "#1570EF"
                     }}
-                />
+                >
+                  Send message
+                </button>
               </div>
           </form>
         </div>
